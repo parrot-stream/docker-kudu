bb862f5c0768fe9fb331a4e94969922175b05d68
Matteo Capitanio <matteo.capitanio@cdp.it>
2018-09-28 09:16:40 +0200

    Removed Zookeeper dependency

--- bin/supervisord-bootstrap.sh
+++ bin/supervisord-bootstrap.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-/wait-for-it.sh zookeeper:2181 -t 120
-rc=$?
-if [ $rc -ne 0 ]; then
-    echo -e "\n--------------------------------------------"
-    echo -e "      Zookeeper not ready! Exiting..."
-    echo -e "--------------------------------------------"
-    exit $rc
-fi
-
 /start-kudu.sh
